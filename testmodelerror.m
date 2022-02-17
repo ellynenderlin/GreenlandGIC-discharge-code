dc9e82fd39d3e52c3484382e8fb91f86bd5312e7
Ellyn Enderlin <ellynenderlin@boisestate.edu>
2022-02-17 14:21:36 -0700

    Formatted to remove extra files
    Moved files that are in progress or old before making public.

--- testmodelerror.m
+++
@@ -1,9 +0,0 @@
-function error = testmodelerror(modelfitobj, testdata)
-% Evaluate a model's error (residual) using cross-validation testing data
-% INPUT:    modelfitobj = the fit object from MATLAB fit function
-%           testdata = matrix with x-values in column 1 and y-values in column 2
-% OUTPUTS:  error = average residual
-% Syntax: error = testmodelerror(modelfitobj, traindata)
-y=testdata(:,2); % grav the actual y-values
-ymod=feval(modelfitobj, testdata(:,1)); % evaluate model at the training data x-values
-error=mean(abs(y-ymod)); % calculate RMSE
