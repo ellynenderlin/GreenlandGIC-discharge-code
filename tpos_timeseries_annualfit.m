3546f8fac48c0742ccd20c7bfd289d6bb4c1849e
Ellyn Enderlin <ellynenderlin@boisestate.edu>
2022-06-10 14:03:33 -0600

    cleaned-up prelim regional terminus plots

--- tpos_timeseries_annualfit.m
+++ tpos_timeseries_annualfit.m
@@ -4,15 +4,10 @@
 % Dependencies: getTestTrain.m, testmodelerror.m, Financial Toolbox
 clear all; close all;
 
-% %load GIMP masks as background
-cd /Users/ellynenderlin/Research/miscellaneous/GIMP
-% load GIMPmasks.mat
+%load the average ITS_LIVE annual velocity map as background
+cd /Users/ellynenderlin/Research/miscellaneous/Greenland-ITSLIVE_1985-2018/
 map_fig = figure; set(map_fig,'position',[50 50 600 1200]); 
-% imagesc(GIMP.x,GIMP.y,2*GIMP.ice+GIMP.land); axis xy equal; 
-% map_cmap = [1 1 1; 0.5 0.5 0.5; 0 1 1]; colormap(map_cmap); hold on;
-
-cd ../Greenland-ITSLIVE_1985-2018/
-[A,R] = geotiffread('Greenland_ITSLIVEavg_1985-2018.tif');
+[A,R] = readgeoraster('Greenland_ITSLIVEavg_1985-2018.tif');
 vx = R.XWorldLimits(1)+0.5*R.CellExtentInWorldX:R.CellExtentInWorldX:R.XWorldLimits(2)-0.5*R.CellExtentInWorldX;
 vy = R.YWorldLimits(2)-0.5*R.CellExtentInWorldY:-R.CellExtentInWorldY:R.YWorldLimits(1)+0.5*R.CellExtentInWorldY;
 v = A; clear A R;
@@ -27,7 +22,7 @@ set(gca,'ylim',[-3.35e6 -0.65e6],'xlim',[-6.5e5 8.5e5],...
 xlabel('Easting (km) ','fontsize',20); ylabel('Northing (km) ','fontsize',20); 
 
 %plot the glacier locations
-cd /Users/ellynenderlin/Research/NASA_Greenland-Periph-Mapping/centerlines/
+cd /Users/ellynenderlin/Research/NASA_GreenlandPeriph-Mapping/
 load Greenland_GIC_centerlines.mat
 for i = 1:length(term)
     X(i) = nanmean(term(i).X); Y(i) = nanmean(term(i).Y); Box(i) = term(i).BoxID;
@@ -41,20 +36,23 @@ CErefs = [311:1:500];
 SErefs = [64:1:310];
 
 % set path to folder with filtered terminus position timeseries
-tspath = '/Users/ellynenderlin/Research/NASA_Greenland-Periph-Mapping/PeripheralGICs_timeseries/';
+tspath = '/Users/ellynenderlin/Research/NASA_GreenlandPeriph-Mapping/terminus_timeseries/';
 cd_to_dir = ['cd ',tspath]; eval(cd_to_dir);
 flowline = '50'; ts_files = dir(['*flowline',flowline,'.csv']);
 
 %loop through the data and plot according to region
-% ts_fig = figure; set(ts_fig,'position',[850 50 800 1200]);
-% subW = subplot(5,1,1); subN = subplot(5,1,2); subNE = subplot(5,1,3); subCE = subplot(5,1,4); subSE = subplot(5,1,5); 
 Wlims = []; Nlims = []; NElims = []; CElims = []; SElims = []; 
 Wslope = NaN(length(ts_files),7); Nslope = NaN(length(ts_files),7); NEslope = NaN(length(ts_files),7); CEslope = NaN(length(ts_files),7); SEslope = NaN(length(ts_files),7); 
 Wintercept = NaN(length(ts_files),7); Nintercept = NaN(length(ts_files),7); NEintercept = NaN(length(ts_files),7); CEintercept = NaN(length(ts_files),7); SEintercept = NaN(length(ts_files),7); 
 Waccel = NaN(length(ts_files),7); Naccel = NaN(length(ts_files),7); NEaccel = NaN(length(ts_files),7); CEaccel = NaN(length(ts_files),7); SEaccel = NaN(length(ts_files),7); 
-%create a video that loops through the time series plots
-term_timeseries = VideoWriter('GreenlandGIC-auto-termchange-video.avi'); term_timeseries.FrameRate=5;
-open(term_timeseries); 
+
+%% create a video that loops through the time series plots
+
+%uncomment to make a video instead of a fig
+% term_timeseries = VideoWriter('GreenlandGIC-auto-termchange-video.avi'); term_timeseries.FrameRate=5;
+% open(term_timeseries); 
+
+%loop through terminus timeseries and plot
 for i = 1:length(ts_files)
     
     % identify the timeseries to plot
@@ -74,14 +72,14 @@ for i = 1:length(ts_files)
     day1 = datetime('2013-01-01','InputFormat','yyyy-MM-dd');  % grab first day of 2013
     t_tpos = zeros(1, numel(tpos_dates)); % create vector to store the runoff timepoints (days after 2013-01-01)
     for b=1:length(tpos_dates)
-        t_tpos(b) = daysact(day1, tpos_dates(b)); % grab number of days after day 1
+        t_tpos(b) = datenum(tpos_dates(b)) - datenum(day1); % grab number of days after day 1
     end
     
     % 3) FOURIER MODEL-FITTING
     % OPTION A: Fit 3 Fourier curves to 90% of the data, auto choose the model with the lowest error
     pTrain = 0.9; % set percent training data
     [tpos_train, tpos_test] = getTrainTest([t_tpos' tpos], pTrain); % split into test and training datasets (use getTestTrain.m)
-    n_tpos = round(daysact(min(tpos_dates), max(tpos_dates))/365); % grab number of years of tpos data
+    n_tpos = round((max(datenum(tpos_dates))-min(datenum(tpos_dates)))/365); % grab number of years of tpos data
     
     % try fourier fits with 3 different number of terms to tpos data
     fouriern=strcat('fourier',string(n_tpos)); % # terms = # years of data
@@ -123,36 +121,36 @@ for i = 1:length(ts_files)
 %             subplot(subW); 
             Wlims = [Wlims; max(yhat)-min(yhat)];
             Wslope(i,k-2013) = flin.p1; Wintercept(i,k-2013) = flin.p2;
-            if (29+(k-2014+1)) <= length(term(boxref).fluxVelavg)
-                Waccel(i,k-2013) = (term(boxref).fluxVelavg(29+(k-2014+1))-term(boxref).fluxVelavg(29+(k-2014)));
+            if (29+(k-2014+1)) <= length(term(boxref).fluxVavg)
+                Waccel(i,k-2013) = (term(boxref).fluxVavg(29+(k-2014+1))-term(boxref).fluxVavg(29+(k-2014)));
             end
         elseif str2num(BoxID) >= min(Nrefs) && str2num(BoxID) <= max(Nrefs)
 %             subplot(subN); 
             Nlims = [Nlims; max(yhat)-min(yhat)];
             Nslope(i,k-2013) = flin.p1; Nintercept(i,k-2013) = flin.p2;
-            if (29+(k-2014+1)) <= length(term(boxref).fluxVelavg)
-                Naccel(i,k-2013) = (term(boxref).fluxVelavg(29+(k-2014+1))-term(boxref).fluxVelavg(29+(k-2014)));
+            if (29+(k-2014+1)) <= length(term(boxref).fluxVavg)
+                Naccel(i,k-2013) = (term(boxref).fluxVavg(29+(k-2014+1))-term(boxref).fluxVavg(29+(k-2014)));
             end
         elseif str2num(BoxID) >= min(NErefs) && str2num(BoxID) <= max(NErefs)
 %             subplot(subNE); 
             NElims = [NElims; max(yhat)-min(yhat)];
             NEslope(i,k-2013) = flin.p1; NEintercept(i,k-2013) = flin.p2;
-            if (29+(k-2014+1)) <= length(term(boxref).fluxVelavg)
-                NEaccel(i,k-2013) = (term(boxref).fluxVelavg(29+(k-2014+1))-term(boxref).fluxVelavg(29+(k-2014)));
+            if (29+(k-2014+1)) <= length(term(boxref).fluxVavg)
+                NEaccel(i,k-2013) = (term(boxref).fluxVavg(29+(k-2014+1))-term(boxref).fluxVavg(29+(k-2014)));
             end
         elseif str2num(BoxID) >= min(CErefs) && str2num(BoxID) <= max(CErefs)
 %             subplot(subCE); 
             CElims = [CElims; max(yhat)-min(yhat)];
             CEslope(i,k-2013) = flin.p1; CEintercept(i,k-2013) = flin.p2;
-            if (29+(k-2014+1)) <= length(term(boxref).fluxVelavg)
-                CEaccel(i,k-2013) = (term(boxref).fluxVelavg(29+(k-2014+1))-term(boxref).fluxVelavg(29+(k-2014)));
+            if (29+(k-2014+1)) <= length(term(boxref).fluxVavg)
+                CEaccel(i,k-2013) = (term(boxref).fluxVavg(29+(k-2014+1))-term(boxref).fluxVavg(29+(k-2014)));
             end
         else
 %             subplot(subSE); 
             SElims = [SElims; max(yhat)-min(yhat)];
             SEslope(i,k-2013) = flin.p1; SEintercept(i,k-2013) = flin.p2;
-            if (29+(k-2014+1)) <= length(term(boxref).fluxVelavg)
-                SEaccel(i,k-2013) = (term(boxref).fluxVelavg(29+(k-2014+1))-term(boxref).fluxVelavg(29+(k-2014)));
+            if (29+(k-2014+1)) <= length(term(boxref).fluxVavg)
+                SEaccel(i,k-2013) = (term(boxref).fluxVavg(29+(k-2014+1))-term(boxref).fluxVavg(29+(k-2014)));
             end
         end
         
@@ -186,43 +184,46 @@ for i = 1:length(ts_files)
     clear flin* yhat*;
 end
 figure(map_fig); saveas(map_fig,'GreenlandGIC_terminus-timeseries-coverage-map.png','png');
-close(term_timeseries);
-% set(subW,'ylim',[-1 1]); set(subN,'ylim',[-1 1]); set(subNE,'ylim',[-1 1]); set(subCE,'ylim',[-1 1]); set(subSE,'ylim',[-1 1]); 
 
-%box plots of annual slopes (retreat rates)
+%uncomment line below if making a video
+% close(term_timeseries);
+
+
+%% box plots of annual slopes (retreat rates)
+boxplot_ylim = [-2 2]; boxplot_ytick = [-2:1:2];
 figure; set(gcf,'position',[850 50 600 1200]);
 subplot(5,1,1);
-boxplot(-Wslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-4 4],'ytick',[-4:2:4],'fontsize',18);
+boxplot(-Wslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',[-2:1:2],'fontsize',18);
 subplot(5,1,2);
-boxplot(-Nslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-4 4],'ytick',[-4:2:4],'fontsize',18);
+boxplot(-Nslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',[-2:1:2],'fontsize',18);
 subplot(5,1,3);
-boxplot(-NEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-4 4],'ytick',[-4:2:4],'fontsize',18);
+boxplot(-NEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',[-2:1:2],'fontsize',18);
 subplot(5,1,4);
-boxplot(-CEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-4 4],'ytick',[-4:2:4],'fontsize',18);
+boxplot(-CEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',[-2:1:2],'fontsize',18);
 subplot(5,1,5);
-boxplot(-SEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-4 4],'ytick',[-4:2:4],'fontsize',18);
+boxplot(-SEslope,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',[-2:1:2],'fontsize',18);
 xlabel('Year','fontsize',20); 
-ylbl = ylabel('Terminus retreat rate (m/d)','fontsize',20); set(ylbl,'position',[0.1071 30 -1]);
+ylbl = ylabel('Terminus retreat rate (m/d)','fontsize',20); set(ylbl,'position',[0.1071 15 -1]);
 saveas(gcf,'GreenlandGIC_terminus-retreatrate_boxplots.png','png');
 
-%box plots of inter-annual speed change
+%% box plots of inter-annual speed change
+boxplot_ylim = [-13 13]; boxplot_ytick = [-12:4:12];
 figure; set(gcf,'position',[1250 50 600 1200]);
 subplot(5,1,1);
-boxplot(Waccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-13 13],'ytick',[-12:4:12],'fontsize',18);
+boxplot(Waccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',boxplot_ytick,'fontsize',18);
 subplot(5,1,2);
-boxplot(Naccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-13 13],'ytick',[-12:4:12],'fontsize',18);
+boxplot(Naccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',boxplot_ytick,'fontsize',18);
 subplot(5,1,3);
-boxplot(NEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-13 13],'ytick',[-12:4:12],'fontsize',18);
+boxplot(NEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',boxplot_ytick,'fontsize',18);
 subplot(5,1,4);
-boxplot(CEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-13 13],'ytick',[-12:4:12],'fontsize',18);
+boxplot(CEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',boxplot_ytick,'fontsize',18);
 subplot(5,1,5);
-boxplot(SEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',[-13 13],'ytick',[-12:4:12],'fontsize',18);
+boxplot(SEaccel,'Labels',{'2014', '2015','2016','2017','2018','2019','2020'}); set(gca,'ylim',boxplot_ylim,'ytick',boxplot_ytick,'fontsize',18);
 xlabel('Year','fontsize',20); 
 ylbl = ylabel('Acceleration (m/yr^2)','fontsize',20); set(ylbl,'position',[0.1071 49 -1]);
 saveas(gcf,'GreenlandGIC_terminus-acceleration_boxplots.png','png');
 
-%plot regional median & IQR (or MAD) terminus change rate time series on left axis
-%and velocity time series on the right axis
+%% plot regional median & IQR (or MAD) terminus change rate time series on left axis and velocity time series on the right axis
 figure; set(gcf,'position',[1050 50 600 1200]);
 subplot(5,1,1);
 yyaxis left; 
